ec336c5c7da43da0b2dc0584834f8edbffa66b7a
adamorucu <adamdorucu@gmail.com>
2021-10-17 21:56:30 +0200

    almost ready

--- tsne.py
+++ tsne.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import numpy as np
+import warnings
 
 def m(t):
     """Momentum"""
@@ -13,7 +14,7 @@ def perp(condi_matr):
     ent = -np.sum(condi_matr * np.log2(condi_matr), 1)
     return 2 ** ent
 
-def search(func, goal, tol=1e-8, max_iters=100, lowb=1e-20, uppb=10000):
+def search(func, goal, tol=1e-10, max_iters=1000, lowb=1e-20, uppb=10000):
     """Binary search algorithm used to find a sigma that gives desired perplexity"""
     for _ in range(max_iters):
         guess = (uppb + lowb) / 2.
@@ -27,7 +28,7 @@ def search(func, goal, tol=1e-8, max_iters=100, lowb=1e-20, uppb=10000):
         if np.abs(val - goal) <= tol:
             return guess
 
-    print(f"Exceeded max iterations, returning {guess} with value {val}")
+    warnings.warn(f"\nSearch couldn't find goal, returning {guess} with value {val}")
     return guess
 
 def p_conditional(dists, sigmas):
@@ -46,8 +47,10 @@ def find_sigmas(dists, perplexity):
     return found_sigmas
 
 def p_joint(X, perp, verbose):
+    """Used to solve the crowdint problem"""
     N = X.shape[0]
     dists = pairwise_distances(X)
+    # print('Size', dists.nbytes)
     sigmas = find_sigmas(dists, perp)
     if(verbose):
         print('Sigmas:', sigmas)
@@ -55,12 +58,14 @@ def p_joint(X, perp, verbose):
     return (p_cond + p_cond.T) / (2. * N)
 
 def q_joint(y): 
+    """Eq. 4"""
     dists = pairwise_distances(y)
     nom = 1 / (1 + dists)
     np.fill_diagonal(nom, 0.)
     return nom / np.sum(np.sum(nom))
 
 def gradient(P, Q, y):
+    """Calculates the gradients used for the gradient descent"""
     (n, no_dims) = y.shape
     pq_diff = P - Q
     y_diff = np.expand_dims(y,1) - np.expand_dims(y,0) 
@@ -70,6 +75,7 @@ def gradient(P, Q, y):
     return 4 * (np.expand_dims(pq_diff, 2) * y_diff * np.expand_dims(aux,2)).sum(1)
 
 def tsne(X, ydim=2, T=1000, l=500, perp=30, verbose=False):
+    """t-distributed stochastic neighbor embedding algothim"""
     N = X.shape[0]
     P = p_joint(X, perp, verbose)
 
@@ -85,3 +91,21 @@ def tsne(X, ydim=2, T=1000, l=500, perp=30, verbose=False):
         if verbose and t % 10 == 0:
             Q = np.maximum(Q, 1e-12)
     return y
+
+def tsne_gif(X, ydim=2, T=1000, l=500, perp=30, verbose=False):
+    """Only used to create the gif for the presentation"""
+    N = X.shape[0]
+    P = p_joint(X, perp, verbose)
+
+    Y = []
+    y = np.random.normal(loc=0.0, scale=1e-4, size=(N,ydim))
+    Y.append(y);  Y.append(y)
+
+    for t in range(T):
+        Q = q_joint(Y[-1])
+        grad = gradient(P, Q, Y[-1])
+        y = Y[-1] - l*grad + m(t)*(Y[-1] - Y[-2])
+        Y.append(y)
+        if verbose and t % 10 == 0:
+            Q = np.maximum(Q, 1e-12)
+    return Y
